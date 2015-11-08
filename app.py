9fc4b4918a1739dfddac75c4ac4da4813917eb72
ItsMrLin <JerryLin1206@Gmail.com>
2015-11-08 00:30:31 -0500

    page tempate

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import requests
-from flask import Flask, request, send_from_directory, url_for
 import os
+from flask import Flask, request, send_from_directory, url_for, render_template
 from settings import APP_STATIC
 
 app = Flask(__name__, static_folder='static', static_url_path='')
@@ -12,18 +12,20 @@ def index():
 @app.route('/discover')
 def discover():
 	# return requests.get('http://api.v3.factual.com/t/places-us?KEY=fJ1pTwv6Pbk8TamZ5T6wBoz1eeZT9dWagezor5sM&filters={"category_ids":{"$includes_any":[171, 445]}}&geo={"$circle":{"$center":[33.77, -84.39],"$meters":2000}}').content
-
-    return 'Hello discover!'
+    return render_template('discover.html')
 
 @app.route('/saving')
 def saving():
-    return 'Hello saving!'
-    from flask import Flask
+    return render_template('saving.html')
 
 @app.route('/static/<path:path>')
-def send_js(path):
+def get_static(path):
 	with open(os.path.join(APP_STATIC, path)) as f:
 		return f.read()
+
+@app.route('/api/get_score')
+def get_score(path):
+	return "{}"
 
 
 if __name__ == '__main__':
